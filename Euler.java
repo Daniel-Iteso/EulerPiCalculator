149173b096cbc0f774d83bf9ff7547870805a70a
Daniel_Ite <142466965+Daniel-Iteso@users.noreply.github.com>
2025-02-06 11:43:38 -0600

    Documentation update
    I just commented all the code

--- Euler.java
+++ Euler.java
@@ -1,34 +1,35 @@
 
 public class Euler implements Calculation{
-
 	
-	private int iteration;
-	private double result;
-	private double factor;
+	private int iteration;//Number of iterations >=1
+	private double result;//The result is being saved here
+	private double factor;//Helper to calculate the factor of a number
 	
-	
+	//Constructor
 	public Euler(int iteration) {
 		
 		this.iteration = iteration;
 		
 	}
 	
+	
+	//Getter to obtain the calculation
 	@Override
 	public double calculateValue() {
 		
 		for (int i=0; i<iteration;i++) {
 			
-			if (i == 0) {
+			if (i == 0) {//0's factor is 1
 			
 				factor = 1;
 			
-			}else {
+			}else {//Calculation of any factor greater or equal than 1
 				
 				factor *= i;
 			
 			}
 			
-			result += 1/factor;
+			result += 1/factor;//Value of the Euler series
 			
 		}
 		
--- Pi.java
+++ Pi.java
@@ -1,32 +1,36 @@
 
 public class Pi implements Calculation{
 	
-	private int iteration;
-	private double result;
-	private double odd = 1;
+	private int iteration;//number of iterations >= 1
+	private double result;//The result is being saved here
+	private double odd = 1;//Helper to use only the odd numbers
 	
+	
+	//Constructor
 	public Pi(int iteration) {
 		
 		this.iteration = iteration;
 		
 	}
 
+	
+	//Getter to obtain the value of the calculation
 	@Override
 	public double calculateValue() {
 		for (int i = 0; i<iteration; i++) {
-			if (i%2 == 0) {
+			if (i%2 == 0) {//Since the Leibniz formula for Pi the operation changes every iteration
 			
-				result += 1/odd;
+				result += 1/odd;//When it's even, an addition is made
 				
 			}else {
 				
-				result -= 1/odd;
+				result -= 1/odd;//When it's odd, a subtraction is made
 				
 			}
 			
-			odd += 2;
+			odd += 2;//The helper odd must be the odd numbers corresponding to the iteration
 		}
-		return 4*result;
+		return 4*result;//the result is multiplied by 4
 	}
 	
 	
